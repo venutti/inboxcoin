f0b3a985a5dbe3df535289b84baa8b90aa464619
venutti <lucasvenutti23@gmail.com>
2025-02-09 13:12:28 -0300

    Add api descriptions for swagger

--- src/stellar/stellar.controller.ts
+++ src/stellar/stellar.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Post } from '@nestjs/common';
 import { StellarService } from './stellar.service';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('stellar')
 export class StellarController {
   constructor(private readonly stellarService: StellarService) {}
 
+  @ApiOperation({ summary: 'Only for Tesnet. Create a Stellar account.' })
   @Post('friendbot')
   createAccountWithFriendbot() {
     return this.stellarService.createAccountWithFriendbot();
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Param, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './user.entity';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('user')
 export class UserController {
@@ -11,6 +12,9 @@ export class UserController {
     return this.userService.findAll();
   }
 
+  @ApiOperation({
+    summary: 'Load or create user based on the Stellar public key.',
+  })
   @Post('load-or-create/:publicKey')
   loadOrCreate(@Param('publicKey') publicKey: string): Promise<User | null> {
     return this.userService.loadOrCreate(publicKey);
